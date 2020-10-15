87d4c19856b7484413f8ad2ce74e87f26021447c
Liberbell <liberbell@gmail.com>
2020-10-16 08:56:11 +0900

    manipulate3

--- manipulatemap.go
+++ manipulatemap.go
@@ -1,6 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
 	w := make(map[string]int)
 	w["answer"] = 10
+
+	fmt.Println("The value: ", w["answer"])
+
+	w["anser"] = 20
+	fmt.Println("The value: ", w["answer"])
 }
