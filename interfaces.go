068b17cdd92f707e4dfb33453eecc40b15dd102f
Liberbell <liberbell@gmail.com>
2020-10-19 08:45:34 +0900

    interfaces3

--- interfaces.go
+++ interfaces.go
@@ -1,5 +1,13 @@
 package main
 
+import "math"
+
 type Calcer interface{ Calc() float64 }
 type Square struct{ X, Y float64 }
 type myFloat float64
+
+func main() {
+	var c Calcer
+	f := myFloat(-math.Sqrt2)
+	s := Square{8, 6}
+}
