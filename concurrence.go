147c5a8b7efa93cc56f171e76d13f797e035387d
Liberbell <liberbell@gmail.com>
2020-10-20 08:50:11 +0900

    concurrence2

---
+++ concurrence.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func msg() {
+	for i := 1; i < 5; i++ {
+		time.Sleep(time.Millisecond * 1000)
+		if i > 3 {
+			fmt.Println(i, "Seconds...yawn")
+		}
+	}
+}
