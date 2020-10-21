c39805d2040c2de0be28ad08d8ce14fbbe3caefc
Liberbell <liberbell@gmail.com>
2020-10-22 08:38:55 +0900

    slices2-5

--- slices2.go
+++ slices2.go
@@ -6,5 +6,7 @@ func main() {
 	slice := []int{10, 15, 20, 25}
 	fmt.Println("\nHere is our slice:")
 	fmt.Println("Slice ==", slice)
-	fmt.Println("slice[1:4]", slice[1:4])
+	fmt.Println("Slice[1:4]", slice[1:4])
+	fmt.Println("Slice[:3]", slice[:3])
+	fmt.Println("Slice[2:]", slice[2:])
 }
