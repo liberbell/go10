819253031064ca254d4412356d344fd8f86edcee
Liberbell <liberbell@gmail.com>
2020-10-23 08:55:07 +0900

    slice2-10

--- slices2.go
+++ slices2.go
@@ -48,7 +48,7 @@ func resize(slice []int) []int {
 	return slice
 }
 
-func report() {
+func report(name string, slice []int) {
 	fmt.Println(name, "=", slice)
 	fmt.Println("len", name, len(slice))
 	fmt.Println("cap", name, "==", cap(slice))
